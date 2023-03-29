240eb88595f4560a7330d8f56c56cda036ddd689
hyeokjonghan <75103512+hyeokjonghan@users.noreply.github.com>
2023-03-29 20:24:27 +0900

    # 상세페이지 퍼블리싱
    # 스킨 선택
    # 공격 범위
    # 태그
    # 이름 등 퍼블리싱

--- pages/operator/[_id].js
+++ pages/operator/[_id].js
@@ -2,29 +2,181 @@ import ContentLayout from "@/layout/contentLayout"
 import PageLayout from "@/layout/pageLayout"
 import { useRouter } from "next/router"
 import style from "@/styles/operator/detail.module.scss"
+import { useEffect, useState } from "react"
+import axios from "axios";
 
-export default function OperatorDetail({results}) {
+export default function OperatorDetail({ results }) {
     const router = useRouter()
     const operatorId = router.query._id
-    console.dir(results)
+    const [korFontsize, setKorFontSize] = useState('36px')
+    const [korLetterSpacing, setKorLetterSpacing] = useState('-0.5px')
+    const settingRangegrid = []
+    const [printRangeJsx,setPrintRnageJsx] = useState([])
+    let printRangeItemJsx = []
+    
+    const rangeGrideInfo = {
+        minRow: 0,
+        minCol: 0,
+        maxRow: 0,
+        maxCol: 0,
+        rangeInfo: {}
+    }
+
+    // const testName = "니어 더 래디언트 나이트"
+    const testName = "씬"
+    const testRange = {"_id":"641dacd46f49b72c7a0d3a19","id":"4-3","direction":1,"grids":[{"row":2,"col":2},{"row":2,"col":3},{"row":1,"col":2},{"row":1,"col":3},{"row":1,"col":4},{"row":0,"col":2},{"row":0,"col":3},{"row":0,"col":4},{"row":-1,"col":2},{"row":-1,"col":3},{"row":-1,"col":4},{"row":-2,"col":2},{"row":-2,"col":3}]}
+    useEffect(() => {
+        if(testName.length > 10) {
+            setKorFontSize('30px')
+            setKorLetterSpacing('-4px')
+        }
+    },[testName])
+
+    useEffect(() => {
+        setPrintRnageJsx([])
+        rangeGrideInfo.rangeInfo = testRange
+        testRange.grids.forEach((range) => {
+            if(range.col > rangeGrideInfo.maxCol) {
+                rangeGrideInfo.maxCol = range.col
+            } 
+            if(range.col < rangeGrideInfo.minCol) {
+                rangeGrideInfo.minCol = range.col
+            }
+            if(range.row > rangeGrideInfo.maxRow) {
+                rangeGrideInfo.maxRow = range.row
+            } 
+            if(range.row < rangeGrideInfo.minRow) {
+                rangeGrideInfo.minRow = range.row
+            }
+        })
+
+        
+        for(let row = rangeGrideInfo.minRow; row <= rangeGrideInfo.maxRow; row++) {
+            settingRangegrid[row] = []
+            for(let col = rangeGrideInfo.minCol; col <= rangeGrideInfo.maxCol; col++) {
+                if(row === 0 && col === 0) {
+                    settingRangegrid[row][col] = 'o'
+                } else {
+                    settingRangegrid[row][col] = 0
+                }
+            }
+        }
+
+        rangeGrideInfo.rangeInfo.grids.map((item) => {
+            if(settingRangegrid[item.row][item.col] !== 'o') {
+                settingRangegrid[item.row][item.col] = 1
+            }
+        })
+        
+        
+        for(let row = rangeGrideInfo.minRow; row <= rangeGrideInfo.maxRow; row++) {
+            
+            
+            setPrintRnageJsx((current) => {
+                return [...current, settingRangegrid[row]]
+            })
+            
+        }
+        
+    }, [])
+    
+
+
     return <>
         <PageLayout>
             <ContentLayout>
                 <div className={style.operatorDetailWrap}>
                     {/* 스킨 */}
-                    <div className={style.operatorSkinWrap}>
-                        <div className={style.operatorSkinPreview}>스킨 프리뷰 영역</div>
-                        <div className={style.operatorSkinList}>
-                            스킨 리스트 영역
+                    <div className={style.skinDefaultInfoWrap}>
+                        <div className={style.operatorSkinWrap}>
+                            {/* <div className={style.logoWrap} style={{backgroundImage: `url('/sample/logo_siracusa.png')`}}></div> */}
+                            <div className={style.operatorSkinPreviewWrap}>
+                                <div className={style.operatorSkin} style={{ backgroundImage: `url('/sample/characters/char_336_folivo_epoque%2322.png')` }}></div>
+                            </div>
+
+                            <div className={style.operatorSkinList}>
+                                <ul>
+                                    <li className={style.operatorSkinItem} style={{ backgroundImage: `url('/sample/char_336_folivo.png')` }}></li>
+                                    <li className={style.operatorSkinItem} style={{ backgroundImage: `url('/sample/char_336_folivo_2.png')` }}></li>
+                                    <li className={style.operatorSkinItem} style={{ backgroundImage: `url('/sample/char_336_folivo_epoque%2322.png')` }}></li>
+                                </ul>
+                            </div>
+
+                        </div>
+
+                        <div className={style.defaultInfoWrap}>
+
+                                <div className={style.rankWrap}>
+                                    <div className={style.starwrap}>
+                                        <i className="bi bi-star-fill"></i>
+                                    </div>
+                                    <div className={style.starwrap}>
+                                        <i className="bi bi-star-fill"></i>
+                                    </div>
+                                    <div className={style.starwrap}>
+                                        <i className="bi bi-star-fill"></i>
+                                    </div>
+                                    <div className={style.starwrap}>
+                                        <i className="bi bi-star-fill"></i>
+                                    </div>
+                                    <div className={style.starwrap}>
+                                        <i className="bi bi-star-fill"></i>
+                                    </div>
+                                    <div className={style.starwrap}>
+                                        <i className="bi bi-star-fill"></i>
+                                    </div>
+                                </div>
+                                <div className={style.nameWrap}>
+                                    <div className={style.appellation}>Sence</div>
+                                    <div className={style.name} style={{letterSpacing: korLetterSpacing, fontSize: korFontsize}}>{testName}</div>
+                                </div>
+
+                                <div className={style.defaultInfo}>
+                                    <div className={style.prefession} style={{ backgroundImage: `url('/sample/class_supporter.png')` }}></div>
+                                    <div className={style.rangeWrap}>
+                                        <div className={style.range}>
+                                            
+                                            <table className={style.rangeTable}>
+                                                <tbody>
+                                                    {printRangeJsx.map((row) => {
+                                                        return <>
+                                                            <tr>
+                                                                {
+                                                                    row.map((col) => {
+                                                                        if(col === 1) {
+                                                                            return <><td className={style.rangeTarget}></td></>
+                                                                        } else if (col === 'o') {
+                                                                            return <><td className={style.rangeOrigin}></td></>
+                                                                        } else {
+                                                                            return <><td></td></>
+                                                                        }
+                                                                    })
+                                                                }
+                                                            </tr>
+                                                        </>
+                                                        
+                                                    })}
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                        <div>공격범위</div>
+                                    </div>
+                                    <div className={style.positionTagWrap}>
+                                        <div className={style.positionWrap}>원거리</div>
+                                        <div className={style.tagWrap}>
+                                            <span>누커</span>
+                                            <span>딜러</span>
+                                            <span>범위공격</span>
+                                        </div>
+                                    </div>
+                                </div>
                         </div>
                     </div>
 
+
                     {/* 기본정보 */}
-                    <div className={style.operatorBasicInfoWrap}>
+                    {/* <div className={style.operatorBasicInfoWrap}>
                         <div>
-                            <div>
-                                이름,등급,직군,서브직군,소속, range, tag
-                            </div>
                             <div>
                                 레벨에 따른 기본 능력치 처리
                                 1정예 2정예 3정예
@@ -56,7 +208,7 @@ export default function OperatorDetail({results}) {
                                 모듈 기본 능력, 모듈 상세 능력
                             </div>
                         </div>
-                    </div>
+                    </div> */}
 
                 </div>
             </ContentLayout>
@@ -65,11 +217,13 @@ export default function OperatorDetail({results}) {
 }
 
 export async function getServerSideProps() {
-    
+
+    // const tagList = await(await axios.get(`http://localhost:8000/api/operator/ss`)).data
+
     return {
         props: {
-            results:{
-                
+            results: {
+
             }
         }
     }
